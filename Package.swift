a21572ef96faa9f3faa2a8e1e8964e7e713b9ad5
John Florian <developer@johnflorian.com>
2024-06-25 10:09:25 -0700

    Initial commit after fork/clone and upgrade to swift-tools-version:5.10

--- Package.swift
+++ Package.swift
@@ -1,4 +1,26 @@
+// swift-tools-version:5.10
 import PackageDescription
+
 let package = Package(
-  name: "LicenseParser"
+    name: "LicenseParser",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "LicenseParser",
+            targets: ["LicenseParser"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "LicenseParser",
+            dependencies: []),
+        .testTarget(
+            name: "LicenseParserTests",
+            dependencies: ["LicenseParser"]),
+    ]
 )
